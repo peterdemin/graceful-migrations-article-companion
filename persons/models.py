8ff7c343c3da47c600b32b38c5320367ef6ee3ea
Petr <petr.demin@nih.gov>
2018-06-18 16:29:19 -0400

    add schema migration

--- persons/models.py
+++ persons/models.py
@@ -4,3 +4,4 @@ from django.db.models import Model, CharField
 class Person(Model):
     first_name = CharField(max_length=64)
     last_name = CharField(max_length=64)
+    full_name = CharField(max_length=128, null=True)
