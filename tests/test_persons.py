86c70a9cc817d7b2d57b377ae4eee6c80c5d8a35
Petr <petr.demin@nih.gov>
2018-06-19 16:02:32 -0400

    change test to use full_name

--- tests/test_persons.py
+++ tests/test_persons.py
@@ -4,6 +4,6 @@ from persons.models import Person
 
 
 @pytest.mark.django_db
-def test_full_name_is_populated():
+def test_full_name_is_preserved():
     person = Person.objects.create(full_name="John Doe")
     assert person.full_name == "John Doe"
